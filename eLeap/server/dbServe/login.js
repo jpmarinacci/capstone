e53150dc5ccf67e36aacfd27869c72a5c9590858
Aren Kasner <Kasnerkid1@comcast.net>
2018-03-17 20:31:54 -0700

    fixed login make the button call the login route

--- eLeap/server/dbServe/login.js
+++ eLeap/server/dbServe/login.js
@@ -1,23 +1,33 @@
-
 var login = {
 	
 	isUserLoggedIn: function(request, response) { 'use strict';
 		console.log("isUserLoggedIn route called");
 		//login tbd
 		var session = request.session;
-		response.send(true);//or false;
+		if(session.email){
+		response.send(true);
+		}
+		else{response.send(false);
+		}
 	},
 	
 	login: function(request, response) { 'use strict';
 		console.log("login route called");
 		//login tbd
 		var sessions = request.session;
+		sessions.email = "user1@none.com";
 		response.send("logged in");
 	},
 	
 	logout: function(request, response) { 'use strict';
 		console.log("logoff route called");
-		//logout tbd
+		req.session.destroy(function(err) {
+	  		if(err) {
+	    	console.log(err);
+	  		} else {
+	    	res.redirect('/');
+  		}
+	});
 	}
 };
 
