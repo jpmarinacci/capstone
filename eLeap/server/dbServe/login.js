3adf0af3994dd918219f88ca3b9391e8da7f5f5a
JP <jp.marinacci@arczap.com>
2018-03-17 20:09:46 -0700

    login updates

--- eLeap/server/dbServe/login.js
+++ eLeap/server/dbServe/login.js
@@ -1,35 +1,15 @@
-var dbServer = require('../dbServer');
-var mysql = require('mysql');
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.set('views', __dirname + '/views');
+/*app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
-
 app.use(session({secret: 'ssshhhhh'}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));*/
 
-var sess;
-
-app.get('/',function(req,res){
-sess = req.session;
-//Session set when user Request our app via URL
-if(sess.email) {
-/*
-* This line check Session existence.
-* If it existed will do some action.
-*/
-    res.redirect('/admin');
-}
-else {
-    res.render('index.html');
-}
-});
 var login = {
-
 	
 	isUserLoggedIn: function(request, response) { 'use strict';
 		console.log("isUserLoggedIn route called");
