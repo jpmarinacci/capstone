8c1efe3b3919289955881d578bbdc9514e16132f
JP <jp.marinacci@arczap.com>
2018-03-17 20:10:35 -0700

    login updates

--- eLeap/server/dbServe/login.js
+++ eLeap/server/dbServe/login.js
@@ -1,13 +1,3 @@
-var express = require('express');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var app = express();
-
-/*app.set('views', __dirname + '/views');
-app.engine('html', require('ejs').renderFile);
-app.use(session({secret: 'ssshhhhh'}));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));*/
 
 var login = {
 	
