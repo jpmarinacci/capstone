4d6df3d2bfe552731fbc4999237b010462f8dd97
Sunny <etsunny@gmail.com>
2018-05-22 16:52:02 -0700

    datetime format reprocessed

--- eLeap/public/js/utilities/utils.js
+++ eLeap/public/js/utilities/utils.js
@@ -10,6 +10,7 @@ define(['eLeap', 'jquery', 'underscore', 'backbone'],
 function (eLeap, $, _, Backbone) { 'use strict';
 
 	var thisUtils = undefined;
+	var timeFormat;
 	
 	eLeap.own.Utils = Backbone.View.extend({
 		
@@ -18,18 +19,35 @@ function (eLeap, $, _, Backbone) { 'use strict';
 		
 		dateTimeToDisplay: function(dateTime) {
 			var dateObj = new Date(dateTime);
-			var dateDisplay = this.convertDateDisplay(dateObj);
-			var timeDisplay = this.convertTimeDisplay(dateObj);
+			//var dateDisplay = this.convertDateDisplay(dateObj);
+			var dateDisplay = this.formatDate(dateObj);
 			return dateDisplay;
 		},
+
+        formatDate: function (date) {
+			var monthNames = [
+				"January", "February", "March",
+				"April", "May", "June", "July",
+				"August", "September", "October",
+				"November", "December"
+			];
+
+			var day = date.getDate();
+			var monthIndex = date.getMonth();
+			var year = date.getFullYear();
+			var hour = date.getHours();
+			var min = date.getMinutes();
+
+			return monthNames[monthIndex] + ' ' + day + ' ' + year + ',' + hour + ':' + min;
+    	},
 		
-		convertDateDisplay: function(dateObj) {
-			return dateObj;
-		},
-		
-		convertTimeDisplay: function(dateObj) {
-			return dateObj;
-		}
+		// convertDateDisplay: function(dateObj) {
+		// 	return (dateObj);
+		// },
+		//
+		// convertTimeDisplay: function(dateObj) {
+		// 	return dateObj;
+		// }
 	});
 	
 	function singleton() {
