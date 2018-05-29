3741975b16f8e82de75559aab0e3e9de3670c713
JP <jp.marinacci@arczap.com>
2018-05-28 23:08:36 -0700

    sproc updates

--- db/sprocs/sprocGetOppID.sql
+++ db/sprocs/sprocGetOppID.sql
@@ -3,26 +3,31 @@ CREATE DEFINER=`eLeapisit`@`%` PROCEDURE `sprocGetOppId`(in oppid_v int, in pers
 BEGIN
 
 DECLARE joinedCount int DEFAULT 0;
+DECLARE personJoined tinyInt DEFAULT 0;
 
-SELECT 'success' AS 'status'; /* Aren - please update this sproc to return this row - thanks*/
+
 
 SET joinedCount = (SELECT COUNT(filledSeatTable.opportunityId) FROM eLeapData.filledSeatTable
 	WHERE filledSeatTable.opportunityId = oppid_v GROUP BY eLeapData.filledSeatTable.opportunityId);
-IF(joinedCount IS NULL) THEN
-	SELECT * , 0 AS 'availableSeats' FROM eLeapData.opportunityTable 
-	WHERE opportunityTable.opportunityId = oppid_v;
+IF(joinedCount IS NULL || joinedCount = 0) THEN
+	/* none joined */
+	SELECT 'success' AS 'status';
+	SELECT * , 0 AS 'availableSeats' FROM eLeapData.opportunity 
+	WHERE opportunity.opportunityId = oppid_v;
+    
+    SELECT 0 AS 'personJoined';
 ELSE
-	SELECT  * , opportunityTable.totalSeats-COUNT(filledSeatTable.opportunityId) AS 'availableSeats' FROM eLeapData.opportunityTable LEFT OUTER JOIN eLeapData.filledSeatTable ON opportunityTable.opportunityId = filledSeatTable.opportunityId
-	WHERE EXISTS (SELECT * FROM filledSeatTable WHERE filledSeatTable.opportunityId = oppid_v)  AND opportunityTable.opportunityId = oppid_v
+	SELECT 'success' AS 'status';
+    /* calculated available seats*/
+	SELECT * , opportunity.totalSeats-COUNT(filledSeatTable.opportunityId) AS 'availableSeats' FROM eLeapData.opportunity LEFT OUTER JOIN eLeapData.filledSeatTable ON opportunity.opportunityId = filledSeatTable.opportunityId
+	WHERE EXISTS (SELECT * FROM filledSeatTable WHERE filledSeatTable.opportunityId = oppid_v)  AND opportunity.opportunityId = oppid_v
 	GROUP BY filledSeatTable.opportunityId;
+    
+    /* is personId_v joined */
+	SELECT COUNT(filledSeatTable.opportunityId) AS 'personJoined' FROM eLeapData.filledSeatTable 
+	WHERE filledSeatTable.opportunityId = oppid_v AND filledSeatTable.personId = personId_v;
+    
 END IF;
-
-/*SELECT * , opportunity.totalSeats-COUNT(filledSeatTable.opportunityId) AS 'availableSeats' FROM eLeapData.opportunity JOIN eLeapData.filledSeatTable ON opportunity.opportunityId = filledSeatTable.opportunityId
-WHERE EXISTS (SELECT * FROM filledSeatTable WHERE filledSeatTable.opportunityId = oppid_v)  AND opportunity.opportunityId = oppid_v
-GROUP BY filledSeatTable.opportunityId;
-SELECT * ,0 AS 'availableSeats' FROM eLeapData.opportunity JOIN eLeapData.filledSeatTable ON opportunity.opportunityId = filledSeatTable.opportunityId
-WHERE ((opportunity.totalSeats-COUNT(filledSeatTable.opportunityId))>1)  AND opportunity.opportunityId = oppid_v
-GROUP BY opportunity.opportunityId;*/
 
 END$$
 DELIMITER ;
