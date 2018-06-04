3786dc3fe783a4594400550463407ca897d17a0a
JP <jp.marinacci@arczap.com>
2018-06-03 20:01:07 -0700

    persons updates

--- eLeap/public/js/collections/persons.js
+++ eLeap/public/js/collections/persons.js
@@ -10,31 +10,58 @@ define(['underscore', 'backbone', 'eLeap', 'controllers/restServer', 'models/per
 		model: Person,
 		
 		routes: {
-			getAllPersons: "/getAllPersons"
+			getAllPersons: "/getAllPersons",
+			getStudentsForClass: "/getStudentsForClass"
 		},
 		
 		sync: function (method, thisCollection, options) {
 			options = options || {};
 			if(method === 'read') {
-				server.postRoute(this.routes.getAllPersons, this.toJSON(), function (response) {
-					if (response.status && response.status !== "success") {
-						if (options.appError) {
-							options.appError(response);
-						}
-					} else {
-						if (options.success) {
-							if(options.context) {
-								options.call(options.success, context);
-							} else {
-								options.success(response);
+				if(options.classId) {
+					var getStudentsInput = {
+						classId: options.classId,
+						ownerId: options.personId
+					};
+					server.postRoute(this.routes.getStudentsForClass, getStudentsInput, function (response) {
+						if (!response || response.status && response.status !== "success") {
+							if (options.appError) {
+								options.appError(response);
+							}
+						} else {
+							if (options.success) {
+								if(options.context) {
+									options.call(options.success, context);
+								} else {
+									options.success(response);
+								}
 							}
 						}
-					}
-				}, function (error) {
-					if (options.error) {
-						options.error(error);
-					}
-				});
+					}, function (error) {
+						if (options.error) {
+							options.error(error);
+						}
+					});
+				} else {
+					server.postRoute(this.routes.getAllPersons, this.toJSON(), function (response) {
+						if (response.status && response.status !== "success") {
+							if (options.appError) {
+								options.appError(response);
+							}
+						} else {
+							if (options.success) {
+								if(options.context) {
+									options.call(options.success, context);
+								} else {
+									options.success(response);
+								}
+							}
+						}
+					}, function (error) {
+						if (options.error) {
+							options.error(error);
+						}
+					});
+				}
 			}
 		}
 	});
